1a41d73704cb64d41b0278c3afa9c4fc60c46951
SY <seyav29@gmail.com>
2023-03-07 14:22:03 -0500

    2nd commit

--- useSessionStorage.ts
+++ useSessionStorage.ts
@@ -17,10 +17,9 @@ declare global {
 type SetValue<T> = Dispatch<SetStateAction<T>>
 
 function useSessionStorage<T>(key: string, initialValue: T): [T, SetValue<T>] {
-  // Get from session storage then
-  // parse stored json or return initialValue
+  
   const readValue = useCallback((): T => {
-    // Prevent build error "window is undefined" but keep keep working
+  
     if (typeof window === 'undefined') {
       return initialValue
     }
@@ -34,14 +33,12 @@ function useSessionStorage<T>(key: string, initialValue: T): [T, SetValue<T>] {
     }
   }, [initialValue, key])
 
-  // State to store our value
-  // Pass initial state function to useState so logic is only executed once
+  
   const [storedValue, setStoredValue] = useState<T>(readValue)
 
-  // Return a wrapped version of useState's setter function that ...
-  // ... persists the new value to sessionStorage.
+  
   const setValue: SetValue<T> = useEventCallback(value => {
-    // Prevent build error "window is undefined" but keeps working
+  
     if (typeof window == 'undefined') {
       console.warn(
         `Tried setting sessionStorage key “${key}” even though environment is not a client`,
@@ -49,16 +46,16 @@ function useSessionStorage<T>(key: string, initialValue: T): [T, SetValue<T>] {
     }
 
     try {
-      // Allow value to be a function so we have the same API as useState
+  
       const newValue = value instanceof Function ? value(storedValue) : value
 
-      // Save to session storage
+  
       window.sessionStorage.setItem(key, JSON.stringify(newValue))
 
-      // Save state
+  
       setStoredValue(newValue)
 
-      // We dispatch a custom event so every useSessionStorage hook are notified
+  
       window.dispatchEvent(new Event('session-storage'))
     } catch (error) {
       console.warn(`Error setting sessionStorage key “${key}”:`, error)
@@ -67,7 +64,7 @@ function useSessionStorage<T>(key: string, initialValue: T): [T, SetValue<T>] {
 
   useEffect(() => {
     setStoredValue(readValue())
-    // eslint-disable-next-line react-hooks/exhaustive-deps
+  
   }, [])
 
   const handleStorageChange = useCallback(
@@ -92,7 +89,7 @@ function useSessionStorage<T>(key: string, initialValue: T): [T, SetValue<T>] {
 
 export default useSessionStorage
 
-// A wrapper for "JSON.parse()"" to support "undefined" value
+
 function parseJSON<T>(value: string | null): T | undefined {
   try {
     return value === 'undefined' ? undefined : JSON.parse(value ?? '')
